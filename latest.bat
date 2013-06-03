0703bdb2b0f49ab4378d39966b76ca4703783b7a
David Eisner <david.eisner@oriel.oxon.org>
2013-06-03 12:51:34 +0100

    Switch launcher paths

--- latest.bat
+++ latest.bat
@@ -1,10 +1,26 @@
-set HOME=C:\dev\cygwin\home\mbrxhee2
+IF [%HOME%] == [] SET HOME=C:\dev\cygwin\home\mbrxhee2
 cd %HOME%\build\wpfmpdclient
 git fetch origin
 git checkout -b auto1 origin/master
 git branch -D auto
 git checkout -b auto origin/master
 git branch -D auto1
+
+SET HOST=%COMPUTERNAME%
+CALL :LoCase HOST
+
+echo %HOST%
+git log|grep -q %HOST%
+if errorlevel 1 git cherry-pick paths-%HOST%
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild 
-cd WpfMpdClient\bin\Debug\
+xcopy /E /C /Y /I WpfMpdClient\bin\Debug WpfMpdClient\bin\Live
+cd WpfMpdClient\bin\Live\
 .\WpfMpdClient.exe
+
+GOTO:EOF
+
+:LoCase
+:: Subroutine to convert a variable VALUE to all lower case.
+:: The argument for this subroutine is the variable NAME.
+FOR %%i IN ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") DO CALL SET "%1=%%%1:%%~i%%"
+GOTO:EOF
